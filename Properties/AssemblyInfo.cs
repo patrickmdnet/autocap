d104e51047bccf85827eb264c8d1678af7017db2
Patrick <patrick@patrickmd.net>
2020-03-27 23:27:42 -0400

    Add DPI awareness

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AutoCap")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Automatically capture primary screen to PNG file every minute")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AutoCap")]
-[assembly: AssemblyCopyright("Copyright Patrick McCormick © 2016")]
+[assembly: AssemblyCopyright("Copyright Patrick McCormick © 2016-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
